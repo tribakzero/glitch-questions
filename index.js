072a420587b6b7190796666d7d5a54e4014a4286
Alejandro Camarillo <alejandro.camarillo@wizeline.com>
2019-10-11 00:24:57 -0500

    chore: Returns correct output

--- index.js
+++ index.js
@@ -7,7 +7,22 @@ const { getURL, currentTimestamp } = require('./utils');
  * @return {Promise}
 */
 
+const makeProjectURL = (domain, path, line, character) =>
+`https://glitch.com/edit/#!/${domain}?path=${path}:${line}:${character}`
+
+const parseQuestions = questions => {
+  const parsedQuestions = questions.map(question => JSON.parse(question.details));
+  const output = parsedQuestions.map(data => {
+    const { domain, path, line, character, question } = data;
+    const url  = makeProjectURL(domain, path, line, character);
+    return { url, question };
+  });
+  return output;
+}
+
 module.exports = () => {
   var url = 'https://api.glitch.com/projects/questions';
-  return getURL(`${url}?cache=${currentTimestamp()}`);
+  return getURL(`${url}?cache=${currentTimestamp()}`)
+    .then(res => response)
+    .then(parseQuestions);
 }
