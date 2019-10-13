2fff85cf53e59273c6440ca86302338c6fc5c6a7
Alejandro Camarillo <alejandro.camarillo@wizeline.com>
2019-10-13 14:03:29 -0500

    refactor: Adds request-promise-native and improves readability

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 'use strict';
 
-const { getURL, currentTimestamp } = require('./utils');
+const { makeRequest, parseQuestions } = require('./utils');
 
 /**
  * Makes requests to glitch.com questions endpoint and wraps it in a promise
  * @return {Promise}
 */
 
-const makeProjectURL = (domain, path, line, character) =>
-`https://glitch.com/edit/#!/${domain}?path=${path}:${line}:${character}`
-
-const parseQuestions = questions => {
-  const parsedQuestions = questions.map(question => JSON.parse(question.details));
-  const output = parsedQuestions.map(data => {
-    const { domain, path, line, character, question, questionId } = data;
-    const url  = makeProjectURL(domain, path, line, character);
-    return { url, question, questionId };
-  });
-  return output;
-}
-
-module.exports = () => {
-  var url = 'https://api.glitch.com/projects/questions';
-  return getURL(`${url}?cache=${currentTimestamp()}`)
-    .then(parseQuestions);
-}
+module.exports = () =>
+  makeRequest().then(parseQuestions);
--- utils.js
+++ utils.js
@@ -1,28 +1,28 @@
 'use strict';
 
-const https = require('https');
-
-const getURL = (url) => {
-  return new Promise((resolve, reject) => {
-    https.get(url, (response) => {
-      let data = '';
-
-      response.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      response.on('end', () => {
-        resolve(JSON.parse(data));
-      });
-    }).on("error", (error) => {
-      reject(error);
-    });
-  });
-};
+const rp = require('request-promise-native');
 
 const currentTimestamp = () => new Date().getTime();
 
-module.exports = {
-  getURL,
-  currentTimestamp
-}
+const makeRequestURL = () =>
+`https://api.glitch.com/projects/questions?cache=${currentTimestamp()}`;
+
+const makeRequest = () => rp({ uri: makeRequestURL(), json: true });
+
+const makeProjectURL = (domain, path, line, character) =>
+  `https://glitch.com/edit/#!/${domain}?path=${path}:${line}:${character}`;
+
+const getQuestionsDetails = (questions) =>
+  questions.map(question => JSON.parse(question.details));
+
+const constructOutput = ({ domain, path, line, character, question, questionId }) =>
+  ({
+    url: makeProjectURL(domain, path, line, character),
+    question,
+    questionId
+  });
+
+const parseQuestions = questions =>
+  getQuestionsDetails(questions).map(details => constructOutput);
+
+module.exports = { makeRequest, parseQuestions };
